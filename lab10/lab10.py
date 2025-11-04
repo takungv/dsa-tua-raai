a2fdd034f3f3b382f6b5d1185efe1eafaae11373
unknown <lordpanda37@gmail.com>
2025-11-05 01:42:26 +0700

    lab10 done

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -1,2 +1,52 @@
 import os
+INF = float('inf')
 
+with open("lab10/testcase/testtest.txt", "r", encoding="utf-8") as f:
+    data = list(map(int, f.read().split()))
+
+n, m, q = data[0:3]
+index = 3
+
+n, m, q = data[0:3]
+index = 3
+
+# เริ่มต้นตารางเสียงสูงสุด
+dist = [[INF] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    dist[i][i] = 0
+
+# ใส่ข้อมูลเส้นเชื่อม
+for _ in range(m):
+    u, v, w = data[index:index+3]
+    dist[u][v] = w
+    dist[v][u] = w
+    index += 3
+
+# Floyd–Warshall แบบ Minimax
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dist[i][j] = min(dist[i][j], max(dist[i][k], dist[k][j]))
+
+
+#OUTPUT
+# แสดงตารางผลลัพธ์
+print("===== LOWEST DECIBEL TABLE =====")
+for i in range(1, n + 1):
+    row = []
+    for j in range(1, n + 1):
+        if dist[i][j] == INF:
+            row.append("INF")
+        else:
+            row.append(str(int(dist[i][j])))
+    print(" ".join(f"{x:>4}" for x in row))
+print("=========================================================\n")
+
+# แสดงผลของแต่ละคู่เมือง
+for _ in range(q):
+    s, t = data[index:index+2]
+    index += 2
+    if dist[s][t] == INF:
+        print(f"{s} -> {t} : No path")
+    else:
+        print(f"{s} -> {t} : {int(dist[s][t])}")
